abdbb0b31fb6ada04219fde8f330d98844d49a1c
Sugandh Kumar <sugandhkumar9@gmail.com>
2022-02-07 23:49:10 +0530

    index page modification

--- index.php
+++ index.php
@@ -202,7 +202,7 @@ include 'db.php';
 									<img src="img/important-notice.png" alt="welcome apbiharpower" width="360px">
 								</div>
 								<div class="term-content">
-									<div style="background-color:#5ECE4A;padding:10px 10px 10px 20px;">
+									<div style="background-color: #0ad39b;padding:10px 10px 10px 20px;">
 										<h3 style="color:white;">Important Notice:-</h3>
 									</div><br>
 									<ul style="margin-left:20px;">
