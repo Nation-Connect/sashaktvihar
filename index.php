b9de1fa3366f2f5e0ce8b5f4ed2bee7b1db5b5b8
Sugandh Kumar <sugandhkumar9@gmail.com>
2022-02-10 13:00:11 +0530

    index page modification

--- index.php
+++ index.php
@@ -191,7 +191,7 @@ include 'db.php';
                                                                 <a href="#" class="text-body">
                                                                     <div class="card bg-warning mb-3" style="padding: 15px;">
                                                                         <div class="card-body">
-                                                                            <i class="fa fa-tachometer fa-4x "></i></br>
+                                                                            <img src="img/job-opportunities"></br>
                                                                             <small class="text-secondary">Mileage (upto)</small>
                                                                             <h5>Full Service</h5>
                                                                         </div>
