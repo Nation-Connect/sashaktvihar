ba1f5ea003f4671ac2edeec68d31844190c88cf3
Sugandh Kumar <sugandhkumar9@gmail.com>
2022-02-10 13:23:47 +0530

    index page modification

--- index.php
+++ index.php
@@ -188,7 +188,7 @@ include 'db.php';
                                                     ?>
 
                                                             <div class="col-xs-12 col-sm-6 col-md-3" style="margin-bottom: 15px;">
-                                                                <a href="terms.php?id="<?php echo $row["id"]; ?> class="text-body">
+                                                                <a href="terms.php?id=<?php echo $row["id"]; ?>" class="text-body">
                                                                     <div class="card bg-warning mb-3" style="padding: 15px;">
                                                                         <div class="card-body">
                                                                             <img src="img/job-opportunities.png" style="width: 64px;"></br></br>
