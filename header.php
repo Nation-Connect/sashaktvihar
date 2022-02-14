0a2111d779a7de260297e5ee0cd77fec960f67f2
Sugandh Kumar <sugandhkumar9@gmail.com>
2022-02-15 00:14:07 +0530

    index page modification

--- header.php
+++ header.php
@@ -51,7 +51,7 @@
                             <?php
 								if ($result->num_rows > 0) {
                                 while ($row = $result->fetch_assoc()) {
-									echo '<li class="text-break"><a href="article-page.html">'.$row['title'].'</a></li>';
+									echo '<li style="word-wrap: break-word;"><a href="article-page.html">'.$row['title'].'</a></li>';
 								}
 							}
                             ?>
