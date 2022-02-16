6d8b352c518d939b681f83fb0286e644bac2868d
Sugandh Kumar <sugandhkumar9@gmail.com>
2022-02-17 00:22:50 +0530

    team page changed

--- index.php
+++ index.php
@@ -297,7 +297,7 @@ include 'db.php';
                                 <div class="item active">
                                     <div class="col-md-4 col-xs-6 teamitem item">
                                         <div class="teamitem-inner">
-                                            <div class="img-team-outer"><img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" class="img-responsive img-team" height="600" width="600" />
+                                            <div class="img-team-outer"><img src="img/default-pic.jpg" class="img-responsive img-team" height="600" width="600" />
                                             </div>
                                             <p class="team-name">Mr. Kumar</p>
                                             <div class="ourteambtborder"></div>
@@ -310,7 +310,7 @@ include 'db.php';
 
                                     <div class="col-md-4 col-xs-6 teamitem item">
                                         <div class="teamitem-inner">
-                                            <div class="img-team-outer"><img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" class="img-responsive img-team" height="600" width="600" />
+                                            <div class="img-team-outer"><img src="img/default-pic.jpg" class="img-responsive img-team" height="600" width="600" />
                                             </div>
                                             <p class="team-name">Mrs. Kumari</p>
                                             <div class="ourteambtborder"></div>
@@ -323,7 +323,7 @@ include 'db.php';
 
                                     <div class="col-md-4 col-xs-6 teamitem item">
                                         <div class="teamitem-inner">
-                                            <div class="img-team-outer"><img src="https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/271deea8-e28c-41a3-aaf5-2913f5f48be6/de7834s-6515bd40-8b2c-4dc6-a843-5ac1a95a8b55.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7InBhdGgiOiJcL2ZcLzI3MWRlZWE4LWUyOGMtNDFhMy1hYWY1LTI5MTNmNWY0OGJlNlwvZGU3ODM0cy02NTE1YmQ0MC04YjJjLTRkYzYtYTg0My01YWMxYTk1YThiNTUuanBnIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmZpbGUuZG93bmxvYWQiXX0.BopkDn1ptIwbmcKHdAOlYHyAOOACXW0Zfgbs0-6BY-E" class="img-responsive img-team" height="600" width="600" />
+                                            <div class="img-team-outer"><img src="img/default-pic.jpg" class="img-responsive img-team" height="600" width="600" />
                                             </div>
                                             <p class="team-name">Mrs. Kumari</p>
                                             <div class="ourteambtborder"></div>
