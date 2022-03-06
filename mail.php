12a91c7a1045b17e40b422cea404d7d003b63743
Sugandh Kumar <sugandhkumar9@gmail.com>
2022-03-06 14:20:18 +0530

    added mail function in contact page

--- mail.php
+++ mail.php
@@ -34,16 +34,16 @@
         //Server settings
         $mail->SMTPDebug = 0;                      // Enable verbose debug output
         $mail->isSMTP();                                            // Send using SMTP
-        $mail->Host       = 'mail.apbiharpower.in';                    // Set the SMTP server to send through
+        $mail->Host       = 'mail.sashaktvihar.com';                    // Set the SMTP server to send through
         $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'info@apbiharpower.in';                     // SMTP username
-        $mail->Password   = 'Sugandh@123';                               // SMTP password
+        $mail->Username   = 'info@sashaktvihar.com';                     // SMTP username
+        $mail->Password   = 'Sashaktvihar@123';                               // SMTP password
         $mail->SMTPSecure = 'tls';         // Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` also accepted
         $mail->Port       = 587;                                    // TCP port to connect to
     
         //Recipients
-        $mail->setFrom('info@apbiharpower.in', 'Apbiharpower');
-        $mail->addAddress('info@apbiharpower.in', 'Apbiharpower');     // Add a recipient
+        $mail->setFrom('info@sashaktvihar.com', 'Sashakt Vihar');
+        $mail->addAddress('info@sashaktvihar.com', 'Sashakt Vihar');     // Add a recipient
         //$mail->addAddress('ellen@example.com');               // Name is optional
         $mail->addReplyTo($email, $name);
         //$mail->addCC('cc@example.com');
@@ -61,13 +61,13 @@
         
         $mail->ClearAddresses();
         $mail->ClearReplyTos();
-        $mail->addReplyTo('apbiharpower@gmail.com', 'Apbiharpower');
+        $mail->addReplyTo('info@sashaktvihar.com', 'Sashakt Vihar');
         $mail->addAddress($email, $name);  
         $mail->Subject = 'Confirmation mail';
         $mail->Body    = 'Hello <b>'.$name.'</b>,<br><br>&nbsp;We have received your query we will reach out to you shortly.';
         $mail->send();
         //echo 'Message has been sent';
-        header("Location: https://apbiharpower.in/contact-form-thank-you.php");
+        header("Location: https://sashaktvihar.com/contact-form-thank-you.php");
     } catch (Exception $e) {
         echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
     }
