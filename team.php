530eb4b980a6e3f2784995f41bfbfd3c3c0387ba
navneetraj830 <99208356+navneetraj830@users.noreply.github.com>
2022-04-19 00:50:19 +0530

    photo

--- team.php
+++ team.php
@@ -115,7 +115,7 @@ include 'db.php';
 									</div>
 								</li>
 								<li class="transition">
-									<div class="wrapper"> <img class="transition" src="img/team/hr.png">
+									<div class="wrapper"> <img class="transition" src="img/team/h.png">
 										<ul class="social">
 											<li> Senior-HR <br>
 												Mr. Banty Kumar</li>
